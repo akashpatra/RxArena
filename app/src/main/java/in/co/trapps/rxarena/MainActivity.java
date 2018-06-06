8a08c68a2189156eaf8a62e0de90382d7985b219
Akash Patra <patraakash9@gmail.com>
2018-06-06 19:55:41 +0530

    Expt using Schedulers

--- app/src/main/java/in/co/trapps/rxarena/MainActivity.java
+++ app/src/main/java/in/co/trapps/rxarena/MainActivity.java
@@ -4,35 +4,79 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "RxArena";
+    private Subscription subscription;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tvResult = findViewById(R.id.textView2);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startRx();
             }
         });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startWithoutRx();
+            }
+        });
+
+
+    }
+
+    private void startWithoutRx() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "In run");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = tvResult.getText().toString();
+                        tvResult.setText(result + " " + 1);
+                    }
+                });
+            }
+        }).start();
     }
 
     private void startRx() {
+        // Create a Observable
+        Observable<Integer> observable = Observable
+                .just(1, 2, 3, 4, 5) // Emits Numbers
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        // Check if the number is odd or not. If odd return true, to emit that object.
+                        return integer % 2 != 0;
+                    }
+                });
+
         // One Way
         Observer<Integer> observer = new Observer<Integer>() {
             @Override
             public void onCompleted() {
                 Log.d(TAG, "In onCompleted");
+                String result = tvResult.getText().toString();
+                tvResult.setText(result + "completed");
             }
 
             @Override
@@ -43,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onNext(Integer integer) {
                 Log.d(TAG, "In onNext, Received: " + integer);
+                String result = tvResult.getText().toString();
+                tvResult.setText(result + " " + integer);
             }
         };
 
@@ -54,15 +100,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        Observable
-                .just(1, 2, 3, 4, 5) // Emits Numbers
-                .filter(new Func1<Integer, Boolean>() {
-                    @Override
-                    public Boolean call(Integer integer) {
-                        // Check if the number is odd or not. If odd return true, to emit that object.
-                        return integer % 2 != 0;
-                    }
-                })
-                .subscribe(onNextAction);
+        subscription = observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscription.unsubscribe();
     }
 }
