84fb17b3d7a50561853ed6a051693b1476825011
Akash Patra <patraakash9@gmail.com>
2018-06-07 16:16:09 +0530

    Expt using Zip Operator

--- app/src/main/java/in/co/trapps/rxarena/MainActivity.java
+++ app/src/main/java/in/co/trapps/rxarena/MainActivity.java
@@ -12,6 +12,7 @@ import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 startWithoutRx();
             }
         });
-
-
     }
 
     private void startWithoutRx() {
@@ -50,8 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        String result = tvResult.getText().toString();
-                        tvResult.setText(result + " " + 1);
+                        updateText(String.valueOf(1));
                     }
                 });
             }
@@ -59,8 +57,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startRx() {
-        // Create a Observable
-        Observable<Integer> observable = Observable
+        // Observables
+//        Observable observable = justObservable();
+        Observable observable = zipObservable();
+
+        // Observers
+        Observer observer = completeObserver();
+//        Action1 action1 = onNextAction();
+        Action1 action1 = onNextZipObjectAction();
+
+        subscription = observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(action1);
+    }
+
+    /**
+     * Create a Observable, with just() operator.
+     *
+     * @return
+     */
+    private Observable justObservable() {
+        return Observable
                 .just(1, 2, 3, 4, 5) // Emits Numbers
                 .filter(new Func1<Integer, Boolean>() {
                     @Override
@@ -69,9 +87,43 @@ public class MainActivity extends AppCompatActivity {
                         return integer % 2 != 0;
                     }
                 });
+    }
 
-        // One Way
-        Observer<Integer> observer = new Observer<Integer>() {
+    /**
+     * Create a Observable, with zip() operator.
+     *
+     * @return
+     */
+    private Observable zipObservable() {
+        Observable<Integer> observable1 = Observable.from(new Integer[]{1, 2, 3, 4, 5});  // Emits integers
+        Observable<String> observable2 = Observable.from(new String[]{"A", "B", "C", "D", "F"});  // Emits alphabets
+        Observable<ZipObject> observable = Observable.zip(observable1, observable2,
+                new Func2<Integer, String, ZipObject>() {
+                    @Override
+                    public ZipObject call(Integer integer, String s) {
+                        Log.d(TAG, "In zipObservable -> Integer: " + integer + ", String: " + s);
+                        ZipObject zipObject = new ZipObject();
+                        zipObject.number = integer;
+                        zipObject.alphabet = s;
+                        return zipObject;
+                    }
+                });
+        return observable;
+    }
+
+    // Class that combines both data streams
+    class ZipObject {
+        int number;
+        String alphabet;
+    }
+
+    /**
+     * A full Observer with all callbacks.
+     *
+     * @return
+     */
+    private Observer completeObserver() {
+        return new Observer<Integer>() {
             @Override
             public void onCompleted() {
                 Log.d(TAG, "In onCompleted");
@@ -87,23 +139,52 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onNext(Integer integer) {
                 Log.d(TAG, "In onNext, Received: " + integer);
-                String result = tvResult.getText().toString();
-                tvResult.setText(result + " " + integer);
+                updateText(String.valueOf(integer));
             }
         };
+    }
 
-        // Second Way
-        Action1<Integer> onNextAction = new Action1<Integer>() {
+    /**
+     * Observer with only onNext Callback for Integer.
+     *
+     * @return
+     */
+    private Action1 onNextAction() {
+        return new Action1<Integer>() {
             @Override
             public void call(Integer integer) {
-                Log.d(TAG, "In onNext, Received: " + integer);
+                Log.d(TAG, "In call, Received: " + integer);
+                updateText(String.valueOf(integer));
             }
         };
+    }
 
-        subscription = observable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(observer);
+    /**
+     * Observer with only onNext Callback for ZipObject.
+     *
+     * @return
+     */
+    private Action1 onNextZipObjectAction() {
+        return new Action1<ZipObject>() {
+            @Override
+            public void call(ZipObject zipObject) {
+                Log.d(TAG, "In zipObjectObserver, Integer: " + zipObject.number + ", String: " + zipObject.alphabet);
+                updateText(zipObject.alphabet, String.valueOf(zipObject.number));
+            }
+        };
+    }
+
+    /**
+     * Show the data in TextView.
+     *
+     * @param args
+     */
+    private void updateText(String... args) {
+        String result;
+        for (String str : args) {
+            result = tvResult.getText().toString();
+            tvResult.setText(result + " " + str);
+        }
     }
 
     @Override
